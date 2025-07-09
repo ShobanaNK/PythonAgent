14ddab22abd115e5fed7bad0fed76dbb25ab1f8d
ShobanaNK <snk.shobana@gmail.com>
2025-07-09 11:33:37 +0530

    favicon

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 import uvicorn
 
 app = FastAPI()
@@ -6,6 +7,10 @@ app = FastAPI()
 @app.get("/")
 async def root_msg():
     return {"message": "Hello World to Appointment Agent"}
+
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    return FileResponse("static/favicon.png")
 
 if __name__ == "__main__":
     uvicorn.run(
